1ea32ab44f1647963bf48f2c9d03119e503ec07b
darrenzheng13 <darrenz@princeton.edu>
2022-05-01 19:24:46 -0400

    colors and person

---
+++ src/components/objects/Person/Person.js
@@ -0,0 +1,20 @@
+import { Group } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import MODEL from "./person.gltf";
+
+class Person extends Group {
+  constructor() {
+    // Call parent Group() constructor
+    super();
+
+    const loader = new GLTFLoader();
+
+    this.name = "person";
+
+    loader.load(MODEL, (gltf) => {
+      this.add(gltf.scene);
+    });
+  }
+}
+
+export default Person;
--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -1,6 +1,7 @@
 import * as Dat from "dat.gui";
+import * as THREE from "three";
 import { Scene, Color } from "three";
-import { Flower, Land, Stage } from "objects";
+import { Flower, Land, Person, Stage } from "objects";
 import { BasicLights } from "lights";
 
 class SeedScene extends Scene {
@@ -11,19 +12,32 @@ class SeedScene extends Scene {
     // Init state
     this.state = {
       gui: new Dat.GUI(), // Create GUI for scene
-      rotationSpeed: 1,
+      rotationSpeed: 10, // speed in which the background color changes
       updateList: [],
     };
 
     // Set background to a nice color
-    this.background = new Color(0x7ec0ee);
 
+    this.backgroundPalette = [
+      new Color(0x4d4dff),
+      new Color(0xc724b1),
+      new Color(0xe0e722),
+      new Color(0xffad00),
+      new Color(0xd22730),
+      new Color(0xdb3eb1),
+      new Color(0x44d62c),
+    ];
+    this.background = this.backgroundPalette[6];
     // Add meshes to scene
-    const land = new Land();
+    // const land = new Land();
     const stage = new Stage();
-    const flower = new Flower(this);
+    const person = new Person();
+    // const flower = new Flower(this);
     const lights = new BasicLights();
-    this.add(land, stage, flower, lights);
+    // position stage
+    stage.position.add(new THREE.Vector3(0, -250, -700));
+    // this.add(land, stage, flower, lights);
+    this.add(stage, person, lights);
 
     // Populate GUI
     this.state.gui.add(this.state, "rotationSpeed", -5, 5);
@@ -35,8 +49,8 @@ class SeedScene extends Scene {
 
   update(timeStamp) {
     const { rotationSpeed, updateList } = this.state;
-    this.rotation.y = (rotationSpeed * timeStamp) / 10000;
-
+    // this.rotation.y = (rotationSpeed * timeStamp) / 10000;
+    this.background = this.backgroundPalette[(rotationSpeed * timeStamp) % 7];
     // Call update for each object in the updateList
     for (const obj of updateList) {
       obj.update(timeStamp);
