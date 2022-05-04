91f0ff4d5a6217641217ced8de512e11dfc2c177
darrenzheng13 <darrenz@princeton.edu>
2022-05-03 20:49:18 -0400

    glb

--- src/components/objects/Person/Person.js
+++ src/components/objects/Person/Person.js
@@ -1,6 +1,7 @@
 import { Group } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import MODEL from "./person.gltf";
+import bin from "./person.bin";
 
 class Person extends Group {
   constructor() {
