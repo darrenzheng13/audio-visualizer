ba7ac452e8ba6bf9c4d2b28755a5d4ff6249cd4f
darrenzheng13 <darrenz@princeton.edu>
2022-05-03 20:59:00 -0400

    remove bins

--- src/components/objects/Person/Person.js
+++ src/components/objects/Person/Person.js
@@ -1,7 +1,6 @@
 import { Group } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import MODEL from "./person.gltf";
-import bin from "./person.bin";
+import MODEL from "./person.glb";
 
 class Person extends Group {
   constructor() {
